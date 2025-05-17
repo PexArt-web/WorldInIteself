21ed4e4b70f822e42134abbab220a944bd6c840f
PexArt-web <pexart74@gmail.com>
2025-05-17 20:20:30 +0100

    WorldInItSelf

--- frontend/src/loaders/letterLoader.js
+++ frontend/src/loaders/letterLoader.js
@@ -1,7 +1,57 @@
-import { getAllLetters } from "@/services/AllLetters";
+import {
+  getAllLetters,
+  getBirthday,
+  getFatherHood,
+  getGoodMorning,
+  getGoodNight,
+  getLoveLetters,
+  getMotherHood,
+  getMotivation,
+  getPoems,
+} from "@/services/AllLetters";
 import { defer } from "react-router-dom";
 
 export const LetterLoader = () => {
   const getLetters = getAllLetters();
   return defer({ getLetters });
+};
+
+export const goodMorningLoader = () => {
+  const goodMorning = getGoodMorning();
+  return defer({ goodMorning });
+};
+
+export const goodNightLoader = () => {
+  const goodNight = getGoodNight();
+  return defer({ goodNight });
+};
+
+export const loveLettersLoader = () => {
+  const loveLetters = getLoveLetters();
+  return defer({ loveLetters });
+};
+
+export const motivationalLoader = () => {
+  const motivational = getMotivation();
+  return defer({ motivational });
+};
+
+export const poemsLoader = () => {
+  const poems = getPoems();
+  return defer({ poems });
+};
+
+export const motherHoodLoader = () => {
+  const motherHood = getMotherHood();
+  return defer({ motherHood });
+};
+
+export const fatherHoodLoader = () => {
+  const fatherHood = getFatherHood();
+  return defer({ fatherHood });
+};
+
+export const birthdayLoader = () => {
+  const birthday = getBirthday();
+  return defer({ birthday });
 };
